29c9ebab7c169bde04d403f49ae6c5af5f4570ea
Nishant Mittal <mittalnishant14@outlook.com>
2019-09-22 05:00:20 +0530

    Get Dev Talks

--- api/COPS_HelloWorld.ts
+++ api/COPS_HelloWorld.ts
@@ -1,7 +1,8 @@
 import { NowRequest, NowResponse } from '@now/node'
 import Telegraf, { ContextMessageUpdate, Middleware } from 'telegraf';
+import axios from "axios"
 
-const bot = new Telegraf(process.env.COPS_HelloWorld_BOT_TOKEN || "")
+const bot = new Telegraf(process.env.COPS_HelloWorld_BOT_TOKEN || "");
 
 bot.use((ctx: ContextMessageUpdate, next) => {
     const start = new Date()
@@ -14,6 +15,20 @@ bot.use((ctx: ContextMessageUpdate, next) => {
 bot.on('new_chat_members', (ctx: ContextMessageUpdate) => {
     const name = ctx.from ? ctx.from.first_name : "fellow nerd"
     return ctx.reply(`Hey ${name}! I'm really interested in you, so can you please introduce yourself?`)
+})
+
+bot.command('DevTalks', (ctx: ContextMessageUpdate) => {
+    axios.get(`https://api.github.com/repos/COPS-IITBHU/DevTalks/issues`).then(async function (response) {
+        await ctx.reply("Upcoming Dev Talks")
+        const result = response.data
+
+        result.forEach(async (element) => {
+            await ctx.reply(element.title)
+            await ctx.reply(element.body)
+        })
+    }).catch(function (error){
+        return ctx.reply(`Some error fetching the upcoming dev talks. ${error}`)
+    })
 })
 
 bot.on('text', (ctx: ContextMessageUpdate) => {
